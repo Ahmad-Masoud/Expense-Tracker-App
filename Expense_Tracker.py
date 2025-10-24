036fe9c6dd059efbc473dc2adc279f1da0704bf3
Muhammad Ahmed Masoud <72744090+Ahmad-Masoud@users.noreply.github.com>
2025-10-24 23:57:14 +0200

    Update Expense_Tracker.py
    Added Categories and displayed labeled total spending using 2d arrays and remaining balance

--- Expense_Tracker.py
+++ Expense_Tracker.py
@@ -1,32 +1,61 @@
+
+#Validates if the Input for amount is not string
 def Digit_Validator(x):
     while True:
-        if x.isdigit():
-            x = int(x)
+        try:
+            x = float(x)
             break
-        else:
-            print("Please Enter Digits: ")
-            x = input("Enter Yearly Income: ")
+        except ValueError:
+            x = input("Please Enter Digits: ")
 
-def main():
+#main
+def main(total_sum):
 
     #User input
     name = input("Enter you name: ")
-    income = input("Enter Yearly Income: ")
+    income = float(input("Enter Yearly Income: "))
     Digit_Validator(income)
     tax = input("Enter Yearly Tax Amount, If unknown enter 'y' to enter Tax percentage: ")
+
     if tax == "y" or tax == "Y":
-        tax_percentage = int(input("Enter Tax Percentage: "))
-        tax = (tax_percentage / 100) * int(income)
+        tax_percentage = float(input("Enter Tax Percentage: "))
+        tax = (tax_percentage / 100) * float(income)
         print("Tax = ",tax)
     else:
         Digit_Validator(tax)
 
+    total_sum += tax
+
+    records.append(['Name', name])
+    records.append(['Income', income])
+    records.append(['Tax', tax])
+
+    user_data(total_sum)
+    print(records)
+
+    return income, total_sum
+
+#User Spending Break down and storing the data in array
+def user_data(total_sum):
+
+    while True:
+        category = input("Enter Category, If Nothing to add enter 'y': ")
+        if category == 'y' or category == 'Y':
+            break
+        else:
+            category_amount = float(input("Enter amount spent: "))
+            total_sum += category_amount
+            records.append([category, category_amount])
+    return total_sum
 
 
+#Public
+records = []
+total_sum = 0.0
 
+if __name__ == "__main__":
+    total_income, total_sum = main(total_sum)
+    balance = total_income - total_sum
+    print("Total Spending: ",total_sum)
+    print("Remaining Balance: ",balance)
 
-
-
-
-if __name__ =="__main__":
-    main()
