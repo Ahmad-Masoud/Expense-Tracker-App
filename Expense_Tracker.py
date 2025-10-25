9adf88d19a3a72f0e80de5fec2c5c350644fdb84
Muhammad Ahmed Masoud <72744090+Ahmad-Masoud@users.noreply.github.com>
2025-10-25 18:19:54 +0200

    Update Expense_Tracker.py
    Saved Records to csv File
    Allowed User to Append Files Later
    Fixed Some Logic Errors 
    Made Program User Friendly

--- Expense_Tracker.py
+++ Expense_Tracker.py
@@ -1,3 +1,16 @@
+import csv
+import os
+
+#Saving User Data in a CSV file
+def Save_to_CSV(filename, records):
+    try:
+        with open(filename, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["Category", "Amount"])
+            writer.writerows(records)
+        print(f"\nData saved successfully to {filename}")
+    except Exception as e:
+        print(f"Error saving to CSV: {e}")
 
 #Validates if the Input for amount is not string
 def Digit_Validator(prompt):
@@ -8,7 +21,7 @@ def Digit_Validator(prompt):
             print("Please enter a valid number.")
 
 #main
-def main(total_sum):
+def User_input(total_sum):
 
     #User input
     name = input("Enter you name: ")
@@ -35,6 +48,8 @@ def main(total_sum):
 
     total_sum = user_data(total_sum)
 
+
+
     return income, total_sum
 
 #User Spending Break down and storing the data in array
@@ -56,13 +71,45 @@ records = []
 total_sum = 0.0
 
 if __name__ == "__main__":
-    total_income, total_sum = main(total_sum)
-    balance = float(total_income) - float(total_sum)
+    while True:
+        print("Enter\n1: Create New File\n2: Append Previous File\n3: Exit ")
+        new_or_append = input("Option: ")
 
-    print("\nRecords:")
-    for r in records:
-        print(r)
+        if new_or_append == "1":
+            total_income, total_sum = User_input(total_sum)
+            balance = float(total_income) - float(total_sum)
+            records.append(["Total Spending: ",total_sum])
+            records.append(["Remaining Balance: ",balance])
+            print("\nRecords:")
+            for r in records:
+                print(r)
 
-    print("Total Spending: ",total_sum)
-    print("Remaining Balance: ",balance)
+            print("Total Spending: ",total_sum)
+            print("Remaining Balance: ",balance)
+            filename = input("Enter File name to Store Data: ")
+            Save_to_CSV(filename, records)
 
+            break
+
+        elif new_or_append == "2":
+            while True:
+                filename = input("Enter File Name to Append or Enter '1' to go back: ")
+                if os.path.exists(filename) == True:
+                    with open(filename, mode = 'a') as file:
+                        records = []
+                        user_data(total_sum)
+                        writer = csv.writer(file)
+                        writer.writerow(records)
+                        print("Data Appended Successfully")
+                        break
+                elif filename == "1":
+                    break
+                else:
+                    print("Invalid Filename")
+
+
+        elif new_or_append == "3":
+            break
+
+        else:
+            print("Invalid Option")
